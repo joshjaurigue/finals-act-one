d7a19fabdb7a4bdd95694154b80e323d4f588ed8
margallostephen <stephenmargallo04@gmail.com>
2024-05-03 21:37:39 +0800

    Fix auth operation

--- auth/routes/web.php
+++ auth/routes/web.php
@@ -17,18 +17,16 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
 
 
-Route::get('/posts/create', function() {
+Route::get('/posts/create', function () {
     return view('posts.create');
 })->name('posts.create');
 
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/posts', [PostController::class, 'index'])->name('home');
+    Route::post('create', [PostController::class, 'store'])->name('create');
+    Route::get('show/{id}', [PostController::class, 'show'])->name('show');
+    Route::get('edit/{id}', [PostController::class, 'edit'])->name('edit');
+    Route::put('update/{id}', [PostController::class, 'update'])->name('update');
+    Route::delete('delete/{id}', [PostController::class, 'destroy'])->name('delete');
 });
-Route::post('create', [PostController::class, 'store'])->name('create');
-Route::get('show/{id}', [PostController::class, 'show'])->name('show');
-Route::get('edit/{id}', [PostController::class, 'edit'])->name('edit');
-Route::put('update/{id}', [PostController::class, 'update'])->name('update');
-Route::delete('delete/{id}', [PostController::class, 'destroy'])->name('delete');
-
-
