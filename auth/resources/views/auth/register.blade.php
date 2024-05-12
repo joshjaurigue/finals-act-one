c0a9edb99623b55ca7b044c2fcad2dbb88149cc4
Jeremy Joshua R. Jaurigue <jauriguejeremyjoshua02@gmail.com>
2024-05-12 23:39:15 +0800

    Duplicated forum app

---
+++ auth/resources/views/auth/register.blade.php
@@ -0,0 +1,42 @@
+<!-- resources/views/auth/registration.blade.php -->
+@extends('layouts.auth')
+
+@section('content')
+<h1>Registration</h1>
+@if (session('success'))
+    <!-- Success Alert -->
+        <div class="alert alert-success" role="alert">
+            {{ session('success') }}
+        </div>
+@endif
+<form action="{{ route('register') }}" method="POST">
+    @csrf
+    <!-- Register form -->
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Enter email here">
+        @error('email')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="name">Username</label>
+        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" placeholder="Enter username here">
+        @error('name')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password here">
+        @error('password')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Re-type password here">
+    </div>
+    <button type="submit" class="btn btn-primary">Register</button>
+</form>
+@endsection
